ec8e3c91b3ffe0c68348833a8b5d20be5c7120a0
Victor Litvintsev <leetwinski@gmail.com>
2019-04-11 22:31:10 +0300

    PAY-000: remove context conversion

--- src/api/payments/service/impl/CommandBasedRepoService.ts
+++ src/api/payments/service/impl/CommandBasedRepoService.ts
@@ -2,32 +2,28 @@ import IRemoteEntityRepoService from "../IRemoteEntityRepoService";
 import ICommand from "../../command/ICommand";
 import { ApiResult } from "../../apitype/index";
 
-type Ctx<T> = { data: T };
-
-export default abstract class CommandBasedRepoService<TEntity, TCreationData, TID>
+export default abstract class CommandBasedRepoService<TEntity, TCreationData, TID = string>
   implements IRemoteEntityRepoService<TEntity, TCreationData, TID> {
     constructor(
-      private createCommand: ICommand<ApiResult<TEntity>, Ctx<TCreationData>>,
-      private getCommand: ICommand<ApiResult<TEntity>, Ctx<TID>>,
-      private deleteCommand: ICommand<ApiResult<void>, Ctx<TID>>,
-      private listCommand: ICommand<ApiResult<TEntity[]>, Ctx<undefined>>,      
+      private createCommand: ICommand<ApiResult<TEntity>, TCreationData>,
+      private getCommand: ICommand<ApiResult<TEntity>, TID>,
+      private deleteCommand: ICommand<ApiResult<void>, TID>,
+      private listCommand: ICommand<ApiResult<TEntity[]>, undefined>
     ) {}
 
-    abstract createContext<T, R extends Ctx<T>>(data: T): R;
-    
     list(): PromiseLike<ApiResult<TEntity[]>> {
-      return this.listCommand.clone().withCtx(this.createContext(undefined)).exec();
+      return this.listCommand.clone().withCtx(undefined).exec();
     }
 
     create(data: TCreationData): PromiseLike<ApiResult<TEntity>> {
-      return this.createCommand.clone().withCtx(this.createContext(data)).exec();
+      return this.createCommand.clone().withCtx(data).exec();
     }
 
     delete(id: TID): PromiseLike<ApiResult<void>> {
-      return this.deleteCommand.clone().withCtx(this.createContext(id)).exec();
+      return this.deleteCommand.clone().withCtx(id).exec();
     }
 
     get(id: TID): PromiseLike<ApiResult<TEntity>> {
-      return this.getCommand.clone().withCtx(this.createContext(id)).exec();
+      return this.getCommand.clone().withCtx(id).exec();
     }
   }
